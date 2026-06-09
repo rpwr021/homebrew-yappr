105c9899054326e070b5629890996b9bbc7dcaa3
rpwr021 <rpwr021@users.noreply.github.com>
2026-06-08 17:11:33 -0700

    yappr 0.1.7

--- Casks/yappr.rb
+++ Casks/yappr.rb
@@ -1,8 +1,8 @@
 cask "yappr" do
   version "0.1.7"
-  sha256 "4eed3b81edb2778a5fa3c6b64496ba5e87cd48dc8c18f4f833877d38b2eb2f22"
+  sha256 "40c11f55d538fe59513f60d05ed112ee22abdfdbbfd727a779f8349499704d7b"
 
-  url "https://github.com/rpwr021/yappr/releases/download/v0.1/Yappr-macos.zip"
+  url "https://github.com/rpwr021/yappr/releases/download/v0.1.7/Yappr-macos.zip"
   name "Yappr"
   desc "Local push-to-talk dictation and voice chat for macOS"
   homepage "https://github.com/rpwr021/yappr"
